111c627e38a05c73302749fdd6ea20666c2b4735
haoxinxi14@gmail.com <haoxinxi14@gmail.com>
2026-04-23 12:05:33 +0700

    chore: add configuration files for webpack and npm and html css boilerplates

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, { mode }) => {
+  const isDevMode = mode === 'development';
+
+  return {
+    mode: mode || 'development',
+    entry: './src/js/index.js',
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '',
+      clean: true,
+    },
+    devtool: isDevMode ? 'inline-source-map' : 'source-map',
+    devServer: {
+      open: true,
+      port: 4000,
+      hot: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+      ],
+    },
+  };
+};
