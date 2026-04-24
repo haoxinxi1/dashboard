bd865151b8dd5ca427946bd8392c7b10ef793a8c
haoxinxi14@gmail.com <haoxinxi14@gmail.com>
2026-04-24 16:33:21 +0700

    chore: add jsdocs in utils

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,12 @@
+/**
+ * Binds a click event listener to an element that calls
+ * the provided handlers in sequence.
+ *
+ * @param {string} selector - CSS selector for the target element.
+ * @param {...function(Event): void} handlers - One or more callback functions to invoke on click.
+ * @throws {TypeError} If the selector does not match any element.
+ */
+
 export function bindClick(selector, ...handlers) {
   document.querySelector(selector).addEventListener('click', (e) => {
     handlers.forEach(fn => fn(e));
